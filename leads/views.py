5a6d59a1814ef03e6f81fff366dacd28709d5135
Agam <iamagam03@gmail.com>
2023-01-26 22:38:02 +0530

    Render context in html

--- leads/views.py
+++ leads/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
 
 def home_page(request):
+    leads = Lead.objects.all()
     context = {
-        "name": "Jay",
-        "age": 65,
+        "leads": leads,
     }
     return render(request, "second_page.html", context)
