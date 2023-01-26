348e450377a9d7392c10315b99fba8b0550d27b9
Agam <iamagam03@gmail.com>
2023-01-26 21:47:43 +0530

    Add admin tables

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User, Agent, Lead
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "is_staff")
+
+
+class LeadAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "age", "agent")
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Agent)
+admin.site.register(Lead, LeadAdmin)
