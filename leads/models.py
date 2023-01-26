41a9d2ec1f6cb65549964579d49244d2632f43b7
Agam <iamagam03@gmail.com>
2023-01-26 12:46:10 +0530

    Add Agent Model

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.PositiveIntegerField(default=1)
+    agent_id = models.ForeignKey("Agent", on_delete=models.SET_NULL, null=True)
+
+
+class Agent(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
