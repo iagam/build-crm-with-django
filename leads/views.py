bd35251a57ab830646c9bef21d399015037f4a5f
Agam <iamagam03@gmail.com>
2023-01-28 21:22:43 +0530

    finish initial leads page

--- leads/views.py
+++ leads/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 from .models import Lead
 
 
-def home_page(request):
+def lead_list(request):
     leads = Lead.objects.all()
     context = {
         "leads": leads,
     }
-    return render(request, "second_page.html", context)
+    return render(request, "leads/lead_list.html", context)
