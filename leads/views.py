542f531eb61554394b33be23250d48375f2c1617
Agam <iamagam03@gmail.com>
2023-01-28 21:48:42 +0530

    Finish lead detail view

--- leads/views.py
+++ leads/views.py
@@ -9,3 +9,11 @@ def lead_list(request):
         "leads": leads,
     }
     return render(request, "leads/lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead,
+    }
+    return render(request, "leads/lead_detail.html", context)
