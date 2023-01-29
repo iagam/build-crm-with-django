06062621b64043a41f885411d983222b20f92971
Agam <iamagam03@gmail.com>
2023-01-29 18:05:31 +0530

    Add created field

--- leads/models.py
+++ leads/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.timezone import now
 
 # Create your models here.
 class User(AbstractUser):
@@ -11,6 +12,7 @@ class Lead(models.Model):
     last_name = models.CharField(max_length=20)
     age = models.PositiveIntegerField(default=1)
     agent = models.ForeignKey("Agent", on_delete=models.SET_NULL, null=True)
+    created = models.DateTimeField(default=now, editable=False)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
